5277c099dbdb2315b3bda57623d6a4be0dee7b85
nattybear <vpark45@gmail.com>
2018-10-16 17:24:33 +0900

    Update main.cpp

--- Math/Problem5/main.cpp
+++ Math/Problem5/main.cpp
@@ -1,6 +1,20 @@
-#include <gsl/gsl>
+#include <iostream>
+using namespace std;
 
-int main(int argc, char* argv[])
-{
-    return 0;
+bool is_prime(int num);
+
+int main() {
+  int limit, i, j;
+  cin >> limit;
+  for (i = 2; i < limit; ++i) {
+    j = i + 6;
+    if (is_prime(i) && is_prime(j))
+      cout << i << ' ' << j << endl;
+  }
+}    
+bool is_prime(int num) {
+  for (int i = 2; i < num; ++i)
+    if (num % i == 0)
+      return false;
+  return true;
 }
