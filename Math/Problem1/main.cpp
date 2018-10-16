9dd3e468254f50e9871d3fcee4bdff3f2516a392
nattybear <vpark45@gmail.com>
2018-10-16 15:58:36 +0900

    Update main.cpp

--- Math/Problem1/main.cpp
+++ Math/Problem1/main.cpp
@@ -1,6 +1,11 @@
-#include <gsl/gsl>
+#include <iostream>
 
-int main(int argc, char* argv[])
-{
-    return 0;
+int main() {
+  unsigned long long int sum;
+  unsigned long int limit, i;
+  cin >> limit;
+  for (i = 3; i <= limit; ++i)
+    if (i % 3 == 0 || i % 5 == 0)
+      sum += i;
+  cout << sum << endl;
 }
