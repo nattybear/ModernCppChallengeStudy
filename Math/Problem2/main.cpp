98e95ae9c4f90af10b37f4e857d6d281b2e16aae
nattybear <vpark45@gmail.com>
2018-10-16 16:19:28 +0900

    Update main.cpp

--- Math/Problem2/main.cpp
+++ Math/Problem2/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
   int num1, num2, max, gcd;
   cin >> num1 >> num2;
-  max = num1 > num2 ? num1 : num2;
+  max = num1 > num2 ? num2 : num1;
   for (int i = 1; i <= max; ++i)
     if (i % num1 == 0 && i % num2 == 0)
       gcd = i;
