399015fa7ae006574dfdebe17a56420799cadd87
nattybear <vpark45@gmail.com>
2018-10-16 16:20:26 +0900

    Update main.cpp

--- Math/Problem2/main.cpp
+++ Math/Problem2/main.cpp
@@ -5,7 +5,7 @@ int main() {
   int num1, num2, min, gcd;
   cin >> num1 >> num2;
   min = num1 < num2 ? num1 : num2;
-  for (int i = 1; i <= max; ++i)
+  for (int i = 1; i <= min; ++i)
     if (i % num1 == 0 && i % num2 == 0)
       gcd = i;
   cout << gcd << endl;
