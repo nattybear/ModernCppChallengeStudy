e3632f5bd7a5d757037136dfb4221035361488cd
nattybear <vpark45@gmail.com>
2018-10-17 09:37:05 +0900

    Update main.cpp

--- Math/Problem6/main.cpp
+++ Math/Problem6/main.cpp
@@ -1,6 +1,14 @@
-#include <gsl/gsl>
+#include <iostream>
+using namespace std;
 
-int main(int argc, char* argv[])
-{
-    return 0;
+int main() {
+  int limit, sum;
+  cin >> limit;
+  for (int i = 12; i < limit; ++i) {
+    for (int j = 1; j < i; ++j)
+      if (i % j == 0)
+        sum += j;
+    if (sum > i)
+      cout << sum << ' ' << sum - i << endl;
+  }
 }
