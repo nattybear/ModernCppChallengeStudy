b5a51d2609356e3193a1c4ee338714458d737e66
vincent <vpark45@gmail.com>
2018-10-17 17:51:34 +0900

    Solve armstrong numbers problem

--- Math/Problem8/main.cpp
+++ Math/Problem8/main.cpp
@@ -1,6 +1,17 @@
-#include <gsl/gsl>
+#include <iostream>
+using namespace std;
 
-int main(int argc, char* argv[])
-{
-    return 0;
+int main() {
+  register int i, j, k;
+  int num, sum;
+  for (i = 1; i < 10; ++i) {
+    for (j = 0; j < 10; ++j) {
+      for (k = 0; k < 10; ++k) {
+        num = 100*i + 10*j + k;
+        sum = i*i*i + j*j*j + k*k*k;
+        if (num == sum)
+          cout << num << endl;
+      }
+    }
+  }
 }
