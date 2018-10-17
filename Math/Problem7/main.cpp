123d2077b73bc39252fc435902a7b9514fab0985
vincent <vpark45@gmail.com>
2018-10-17 14:38:52 +0900

    Write global function "get_sum_of_proper_divisors"

--- Math/Problem7/main.cpp
+++ Math/Problem7/main.cpp
@@ -1,6 +1,24 @@
-#include <gsl/gsl>
+#include <iostream>
+#include <cmath>
+#include <map>
+using namespace std;
 
-int main(int argc, char* argv[])
-{
-    return 0;
+#define MAX 1000000
+
+int get_sum_of_proper_divisors(int num);
+
+int main() {
+  int num;
+  cin >> num;
+  cout << get_sum_of_proper_divisors(num) << endl;
+}
+int get_sum_of_proper_divisors(int num) {
+  int q, r, sum = 1;
+  for (int i = 2; i <= sqrt(num); ++i) {
+    q = num / i;
+    r = num % i;
+    if (r == 0)
+      sum += q+i;
+  }
+  return sum;
 }
