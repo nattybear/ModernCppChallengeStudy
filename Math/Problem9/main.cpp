2c7f2b974b656c0839abbcb9199fc4f083981284
nattybear <vpark45@gmail.com>
2018-10-18 14:39:41 +0900

    Solve prime factors problem

--- Math/Problem9/main.cpp
+++ Math/Problem9/main.cpp
@@ -1,6 +1,35 @@
-#include <gsl/gsl>
+#include <iostream>
+using namespace std;
 
-int main(int argc, char* argv[])
-{
+static int prime_factor = 2;
+
+bool is_prime(int num);
+void next_prime();
+int get_prime_factors(int num);
+
+int main() {
+  int num;
+  cin >> num;
+  get_prime_factors(num);
+}
+bool is_prime(int num) {
+  for (int i = 2; i < num; ++i)
+    if (num % i == 0)
+      return false;
+  return true;
+}
+void next_prime() {
+  while (!is_prime(++prime_factor)) {}
+}
+int get_prime_factors(int num) {
+  if (is_prime(num)) {
+    cout << num << endl;
     return 0;
+  }
+  if (num % prime_factor == 0) {
+    cout << prime_factor << ' ';
+    return get_prime_factors(num / prime_factor);
+  }
+  next_prime();
+  return get_prime_factors(num);
 }
