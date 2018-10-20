03a85a794fdcaf4e89c32ad824edc689a99b2aad
vincent <vpark45@gmail.com>
2018-10-20 22:49:20 +0900

    Solve collatz sequence problem

--- Math/Problem12/main.cpp
+++ Math/Problem12/main.cpp
@@ -8,15 +8,21 @@ static unsigned step;
 unsigned collatz(unsigned num);
 
 int main() {
-  unsigned num;
-  cin >> num;
-  collatz(num);
-  cout << step << endl;
+  unsigned max_step = 0, max_num = 0;
+  for (int i = 3; i <= MAX; ++i) {
+    collatz(i);
+    if (step > max_step) {
+      max_step = step;
+      max_num = i;
+    }
+    step = 0;
+  }
+  cout << max_num << ' ' << max_step << endl;
 }
 unsigned collatz(unsigned num) {
   ++step;
   if (num == 1)
-    return step;
+    return 0;
   if (num % 2 == 0)
     return collatz(num / 2);
   return collatz(num*3 + 1);
