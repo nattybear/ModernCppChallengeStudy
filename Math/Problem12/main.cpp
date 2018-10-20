c0a657fed47c96e0bcfbd3ae7a50711f4320a5f2
vincent <vpark45@gmail.com>
2018-10-20 21:41:15 +0900

    Write global function collatz()

--- Math/Problem12/main.cpp
+++ Math/Problem12/main.cpp
@@ -1,6 +1,23 @@
-#include <gsl/gsl>
+#include <iostream>
+using namespace std;
 
-int main(int argc, char* argv[])
-{
-    return 0;
+#define MAX 1000000
+
+static unsigned step;
+
+unsigned collatz(unsigned num);
+
+int main() {
+  unsigned num;
+  cin >> num;
+  collatz(num);
+  cout << step << endl;
+}
+unsigned collatz(unsigned num) {
+  ++step;
+  if (num == 1)
+    return step;
+  if (num % 2 == 0)
+    return collatz(num / 2);
+  return collatz(num*3 + 1);
 }
