f4b9dffae6fd35f89a6aa0ac0defa99043354249
vincent <vpark45@gmail.com>
2018-10-20 23:35:51 +0900

    Solve the ISBN problem

--- Math/Problem14/main.cpp
+++ Math/Problem14/main.cpp
@@ -1,6 +1,20 @@
-#include <gsl/gsl>
+#include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, char* argv[])
-{
-    return 0;
+bool is_valid_isbn(const string& isbn);
+
+int main() {
+  string isbn;
+  cin >> isbn;
+  if (is_valid_isbn(isbn))
+    cout << "valid" << endl;
+  else
+    cout << "not valid" << endl;
+}
+bool is_valid_isbn(const string& isbn) {
+  int sum = 0;
+  for (int i = 0; i < isbn.size(); ++i)
+    sum += (isbn[i] - '0') * (i + 1);
+  return sum % 11 == 0 ? true : false;
 }
