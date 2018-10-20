6b80f8e91f9180ffe0f5ad05ad7f3ac652358db1
vincent <vpark45@gmail.com>
2018-10-20 20:13:32 +0900

    Modify a parameter for global function to_gray()

--- Math/Problem10/main.cpp
+++ Math/Problem10/main.cpp
@@ -4,14 +4,16 @@ using namespace std;
 
 typedef bitset<5> bit;
 
-bit to_gray(int);
+bit to_gray(const bit&);
 
 int main() {
-  for (int i = 0; i < 32; ++i)
-    cout << i << ' ' << to_gray(i) << endl;
+  for (int i = 0; i < 32; ++i) {
+    bit normal(i);
+    cout << normal << ' ' << to_gray(normal) << endl;
+  }
 }
-bit to_gray(int num) {
-  bit gray, normal(num);
+bit to_gray(const bit& normal) {
+  bit gray;
   gray[4] = normal[4];
   for (int i = 3; i >= 0 ; --i)
     gray[i] = normal[i+1] ^ normal[i];
