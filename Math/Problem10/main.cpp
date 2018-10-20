2357eea3537ca48f927540e2e45c6885af8a4305
vincent <vpark45@gmail.com>
2018-10-20 20:06:50 +0900

    Write global function to_gray()

--- Math/Problem10/main.cpp
+++ Math/Problem10/main.cpp
@@ -1,6 +1,19 @@
-#include <gsl/gsl>
+#include <iostream>
+#include <bitset>
+using namespace std;
 
-int main(int argc, char* argv[])
-{
-    return 0;
+typedef bitset<5> bit;
+
+bit to_gray(int);
+
+int main() {
+  for (int i = 0; i < 32; ++i)
+    cout << i << ' ' << to_gray(i) << endl;
+}
+bit to_gray(int num) {
+  bit gray, normal(num);
+  gray[4] = normal[4];
+  for (int i = 3; i >= 0 ; --i)
+    gray[i] = normal[i+1] ^ normal[i];
+  return gray;
 }
