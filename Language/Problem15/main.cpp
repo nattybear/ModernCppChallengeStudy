88c71804d8dffae7a6b3e99492b6ee2f7f956012
vincent <vpark45@gmail.com>
2018-10-31 17:08:41 +0900

    Add the global function split()

--- Language/Problem15/main.cpp
+++ Language/Problem15/main.cpp
@@ -1,6 +1,25 @@
-#include <gsl/gsl>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
-int main(int argc, char* argv[])
-{
-    return 0;
+#include "ip.h"
+
+vector<string> split(const string& s);
+
+int main() {
+  cout << split("a.b.c.d").size() << endl;
+}
+vector<string> split(const string& s) {
+  int i, j = 0, len = s.length();
+  string temp;
+  vector<string> v;
+  for (i = 0; i < len; ++i)
+    if (s[i] == '.' || i == len - 1) {
+      for (; j < i; ++j)
+        temp += s[j]; 
+      v.push_back(temp);
+      j = i;
+    }
+  return v;
 }
